cf681077c57ba536c1f3919bfea6bd9ba0de6370
Nagarjuna <nagarjuna.gr8@gmail.com>
2019-11-20 16:14:12 +0530

    initial commit

---
+++ src/main/java/com/example/HomePage.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomePage {
+    @GetMapping("/version")
+    String versionMethod()
+    {
+        return "the version is 1.3";
+    }
+}
